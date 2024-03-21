85db81c86b2f6f6b888f266517b6486035e23f6d
YashG92 <95033275+YashG92@users.noreply.github.com>
2024-03-21 22:07:35 +0530

    by default selection
    month timeline and category

--- lib/screens/transaction_screen.dart
+++ lib/screens/transaction_screen.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/widgets/category_list.dart';
 import 'package:expense_tracker/widgets/tab_bar_view.dart';
 import 'package:expense_tracker/widgets/time_line_month.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionScreen extends StatefulWidget {
   const TransactionScreen({super.key});
@@ -11,32 +12,38 @@ class TransactionScreen extends StatefulWidget {
 }
 
 class _TransactionScreenState extends State<TransactionScreen> {
-   String category = '';
-    String monthYear = '';
+  String category = 'All'; // Set default category to "All"
+  String monthYear = DateFormat('MMM y').format(DateTime.now());
   @override
   Widget build(BuildContext context) {
-
-   
-
     return Scaffold(
-      appBar: AppBar(title: Text('Expenses'),),
+      appBar: AppBar(
+        title: Text('Expenses'),
+      ),
       body: Column(
         children: [
-          TimeLineMonth(onChanged: (String? value) { 
-           if (value!=null) {
-             setState((){
-              monthYear = value;
-             });
-           }
-           },),
-          CategoryList(onChanged: (String? value) { 
-            if (value!=null) {
-              setState(() {
-                category = value;
-              });
-            }
-           },),
-           MyTabBarView(category: category, monthYear: monthYear,),
+          TimeLineMonth(
+            onChanged: (String? value) {
+              if (value != null) {
+                setState(() {
+                  monthYear = value;
+                });
+              }
+            },
+          ),
+          CategoryList(
+            onChanged: (String? value) {
+              if (value != null) {
+                setState(() {
+                  category = value;
+                });
+              }
+            },
+          ),
+          MyTabBarView(
+            category: category,
+            monthYear: monthYear,
+          ),
         ],
       ),
     );
